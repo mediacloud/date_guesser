99d7a3df6f392e5e632045571819ffe3a3c710c0
Colin Carroll <colcarroll@gmail.com>
2018-01-27 11:26:39 -0500

    Bump version for release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(requirements_file) as f:
 
 setup(
     name='date_guesser',
-    version='2.0.0',
+    version='2.1.0',
     description='Extract publication dates from web pages',
     long_description=long_description,
     author='Colin Carroll',
